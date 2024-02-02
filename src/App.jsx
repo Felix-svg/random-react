5058486f2af84a2a0e4a4b014f65bf3a22e664f1
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-02-02 13:11:32 +0300

    Update fox images and likes based on click events

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+/* 
+Deliverables
+- Use the `useState` hook for the `image` and `likes` variables
+- When the New Fox button is clicked, use the `handleNewFoxClick` function to fetch a random fox image. Use the image response from fetch to set the image variable.
+- When the Like button is clicked, create an event handler that will increment the number of likes in state. Use the callback version of the state setter function to set the new number of likes.
+*/
+
+function App() {
+  const [image, setImage] = useState("https://randomfox.ca/images/41.jpg");
+
+  const [likes, setLikes] = useState(0);
+
+  function handleNewFoxClick() {
+    fetch("https://randomfox.ca/floof/")
+      .then((response) => response.json())
+      .then(({ image }) => {
+        console.log(image);
+        setImage(image)
+        setLikes(0)    //reset number of likes when a new fox is rendered
+      });
+  }
+
+  function handleLikesClick(){
+    setLikes(likes => likes + 1)
+  }
+
+  return (
+    <div>
+      <h1>🦊 FoxFindr 🦊</h1>
+      <div className="buttons">
+        <button onClick={handleNewFoxClick}>New 🦊 Please</button>
+        <button onClick={handleLikesClick}>Likes: {likes}</button>
+      </div>
+      <img src={image} alt="Random Fox" />
+    </div>
+  );
+}
+
+export default App;
